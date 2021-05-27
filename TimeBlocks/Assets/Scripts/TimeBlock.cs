8cd7b29ae08cc3f7b441bb8a693c2257378e4c39
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2021-05-27 15:03:00 +0800

    2021.5.27

--- TimeBlocks/Assets/Scripts/TimeBlock.cs
+++ TimeBlocks/Assets/Scripts/TimeBlock.cs
@@ -6,15 +6,16 @@ public class TimeBlock
 {
     private string _name;
     private int[] _time;
+    private int _timeRequired;
+    private string _tag;
     private int _priority;
-    private LinkedList<int>_tags;
     private bool _isOver;
     private bool _isFailed;
 
-    public TimeBlock(string name,int[] time,int priority,LinkedList<int> tags) {
+    public TimeBlock(string name,int[] time,string tag,Dictionary tagPriorityList) {
         SetName(name);
         SetTime(time);
-        SetPriority(priority);
+        SetPriority(time,tagPriorityList);
         SetTags(tags);
         _isOver = false;
         _isFailed = false;
@@ -54,20 +55,8 @@ public class TimeBlock
     {
         return _priority;
     }
-    public bool SetPriority(int priority) 
-    {
-        try
-        {
-            _priority=priority;
-            return true;
-        }
-        catch (Exception e)
-        {
-            return false;
-        }
-    }
-    public LinkedList<int> Tags() {
-        return Tags;
+    public string Tag() {
+        return Tag;
     }
     public bool SetTags(LinkedList<int> tags)
     {
@@ -81,14 +70,13 @@ public class TimeBlock
             return false;
         }
     }
-    public boolean update(int[] currentTime) {
-        for (int i=0;i<6;i++) {
-            if (currentTime[i]<_time[i]) { 
-                reuturn true;
-            }
+    public boolean update(int[] currentTime,Dictionary<string,int> tagPriorityList) {
+        int timeRemaining=(currentTime[0]-_time[0])*1030+ (currentTime[1] - _time[1])*30*24*60+ (currentTime[2] - _time[2])*24*60+ (currentTime[3] - _time[3])*60+ (currentTime[4] - _time[4])
+        if (timeRemaining<0) { 
             _isFailed = true;
-            return false;
         }
+        _priority=
+        return false;
     }
     public void checkFinished() {
         _isOver = true;
