32574f22ad1f3138c1054e2145d5eda2b8c3d51f
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2021-08-20 15:25:34 +0800

    2021.8.20

--- TimeBlocks/Assets/Scripts/SQLSaver.cs
+++ TimeBlocks/Assets/Scripts/SQLSaver.cs
@@ -27,23 +27,7 @@ public class SQLSaver : MonoBehaviour
     void Update()
     {
     }
-    void Save() {
-        mySqlConnection = new MySqlConnection(sql);
-        mySqlConnection.Open();
-        Debug.Log("数据库已连接");
-        MySqlCommand cmd = new MySqlCommand("select * from tb_user_info", mySqlConnection);
-        MySqlDataReader reader = cmd.ExecuteReader();
-        while (reader.Read())
-        {
-            for (int i = 0; i < reader.FieldCount; i++)
-            {
-                Debug.Log(reader[i].ToString());
-            }
-        }
-        Debug.Log(reader);
-        mySqlConnection.Close();
-        Debug.Log("数据库关闭");
-    }
+
     public void SignUp(string text, string v, string email)
     {
         mySqlConnection = new MySqlConnection(sql);
