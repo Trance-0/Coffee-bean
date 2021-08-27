644b1698690186a4ea30cb435ebfc168fecb2bfb
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2021-08-27 21:34:18 +0800

    2021.8.27

---
+++ TimeBlocks/Assets/Scripts/Cheat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cheat : MonoBehaviour
+{
+    public InputField input;
+    public CanvasManager canvasManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void changeCanvas() {
+        canvasManager.ChangeCanvas(int.Parse(input.text));
+    }
+}
--- TimeBlocks/Assets/Scripts/FocusManager.cs
+++ TimeBlocks/Assets/Scripts/FocusManager.cs
@@ -1,23 +1,21 @@
-
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 public class FocusManager: MonoBehaviour
 {
     bool isPaused = false;
 
-    void OnGUI()
-    {
-        if (isPaused)
-           Debug.Log("Game paused");
-    }
-
     void OnApplicationFocus(bool hasFocus)
     {
         isPaused = !hasFocus;
+        Debug.Log("Focused");
     }
 
     void OnApplicationPause(bool pauseStatus)
     {
         isPaused = pauseStatus;
+        Debug.Log("Not Focused");
     }
 }
