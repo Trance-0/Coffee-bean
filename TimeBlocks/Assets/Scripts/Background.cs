1ae420401cf1ae92a14e581636e966f8d240ff99
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2021-09-05 08:54:35 +0800

    2021.9.5

---
+++ TimeBlocks/Assets/Scripts/Background.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Background : MonoBehaviour
+{
+    public Image backgroud;
+    public DataManager dataManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroud.color = dataManager.backgroundColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
